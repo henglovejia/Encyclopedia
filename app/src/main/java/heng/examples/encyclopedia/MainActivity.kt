44fe8cccc646b3847fce8a731b89d2f98501b457
zhangheng <zhangheng@bilibili.com>
2021-01-16 00:43:59 +0800

    项目初始化

---
+++ app/src/main/java/heng/examples/encyclopedia/MainActivity.kt
@@ -0,0 +1,11 @@
+package heng.examples.encyclopedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
